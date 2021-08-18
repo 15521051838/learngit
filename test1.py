17664be8391137d285a947f8cf195e7899040b8c
Yfeng Xu <825043354@qq.com>
2021-08-18 20:43:27 +0800

    test

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ def test():
     elif len(args)==2:
         print('Hello, %s!' % args[1])
     else:
-        print('Too many arguments!')
+        print('Too many arguments！!')
 
 if __name__=='__main__':
     test()
